f4ac351cc22801e51cc90d2fbda5c3edf02416dd
Kallol Bairagi <54534189+kaybeesh@users.noreply.github.com>
2026-06-15 09:21:57 +0530

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+
+
+        // tc -> n log n
+        // log n because n = 2^k -1
+        // --> k = log n + 1
+        // if we limit k to 32 ya 64 bit then it becomes constant time
+        // interesting
+
+        // for(int i = 0; i<= n; i++){
+        //     int c = 0;
+        //     int n = i;
+        //     while(n!=0){
+        //         n= n&(n-1);
+        //         c++;
+        //     }
+        //     res.push_back(c);
+        // }
+
+        // return res;
+
+
+        // dp crazy
+        // method 1 = the idea is any number can be represented by
+        // n = 2^k + remainder in binary makes common sense
+        // that means simply number of bits = 1 + number if bits in remainder
+
+
+        // vector<int> dp(n+1);
+        // dp[0]=0;
+        // int off = 1;
+
+        // for(int i=1; i<=n; i++){
+        //     if(off * 2 == i) off = i;
+        //     int rem = i - off;
+        //     dp[i] = 1 + dp[rem];
+        // }
+        // return dp;
+
+        // dp method 2 == use right shift operator
+        // the idea is similar but in terms of set bits 
+        // sb(i) = sb(i >> 1) + (i & 1 : basically rightmost bit because we moved right by 1 bit hahahaha lol)
+        vector<int> dp(n+1);
+        dp[0] = 0;
+
+        for(int i = 1; i<=n; i++){
+            dp[i] = dp[i >> 1] + (i&1);
+        }
+
+        return dp;
+        
+    }
+};
